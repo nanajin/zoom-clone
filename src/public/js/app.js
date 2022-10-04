a5513d1cf2b9afe023711c242b453caaf6a9296e
nanajin <nmj5182@gmail.com>
2022-10-04 19:09:25 +0900

    zoom-clone with socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,45 +1,52 @@
-//frontend 코드
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
+const socket = io();
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = document.querySelector("form");
+const room = document.getElementById("room");
 
-function makeMessage(type, payload){
-  const msg = {type, payload};
-  return JSON.stringify(msg);
+room.hidden = true;
+let roomName;
+
+
+function addMessage(message){
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message; 
+  ul.appendChild(li);
 }
-socket.addEventListener("open", ()=>{
-  console.log("connected to Server");
-})
 
-socket.addEventListener("message",(message)=>{
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-  console.log("New message: ", message.data);
-})
-
-socket.addEventListener("close", ()=>{
-  console.log("Disconnected ");
-})
-
-function handleSubmit(event){
+function handleMessageSubmit(event){
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  const li = document.createElement("li");
-  li.innerText = `You: ${input.value}`;
-  messageList.append(li);
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, ()=>{
+    addMessage(`You: ${value}`);
+  });
   input.value = "";
 }
-
-function handleNickSubmit(event){
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-  input.value = "";
+function showRoom(){
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form")
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+function handleRoomSubmit(event){
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = ""
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", ()=>{
+  addMessage("Someone joined");
+});
+socket.on("bye", ()=>{
+  addMessage("someone left");
+});
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
-import WebSocket from "ws";
-
+import SocketIO from "socket.io";
 const app = express();
 
 app.set("view engine", "pug");
@@ -12,27 +11,49 @@ app.get("/", (req, res)=> res.render("home"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-
-const wss = new WebSocket.Server({server}); //http 서버 위에 웹소켓 서버 만듦
-
-const sockets = [];
-
-wss.on("connection", (socket)=>{
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to browser");
-  socket.on("message",(msg)=>{
-    const message = JSON.parse(msg);
-    switch(message.type){
-      case "new_message":
-        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
+const io = SocketIO(server);
+io.on("connection", (socket)=>{
+  socket.onAny((event)=>{
+    console.log(`Socket Event: ${event}`);
   });
-  socket.on("close",()=>{
-    console.log("Disconnected");
+  socket.on("enter_room", (roomName, done)=> 
+  { 
+    socket.join(roomName); 
+    // console.log(socket.rooms); //어느 방에 있나요 = socket.id
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("disconnecting", ()=>{
+    socket.rooms.forEach(room => socket.to(room).emit("bye"));
+  })
+  socket.on("new_message", (msg, room, done)=>{
+    socket.to(room).emit("new_message", msg);
+    done();
   })
 });
+
+
+
+// const wss = new WebSocket.Server({server}); //http 서버 위에 웹소켓 서버 만듦
+
+// const sockets = [];
+
+// wss.on("connection", (socket)=>{
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+//   console.log("Connected to browser");
+//   socket.on("message",(roomName)=>{
+//     const message = JSON.parse(roomName);
+//     switch(message.type){
+//       case "new_message":
+//         sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+//   socket.on("close",()=>{
+//     console.log("Disconnected");
+//   })
+// });
 
 server.listen(3000, handleListen);
